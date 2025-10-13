5130e3fd602b0c4ce9e4cc06ac9c42ffc4528856
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2025-10-13 20:51:28 +0200

    Build optimized *.ll in run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Compile .rs to .ll
 cargo run -- "examples/example.rs"
+
+# Create alternative optimized version of .ll (allows easier debugging of control flow etc.)
+opt -passes=mem2reg,simplifycfg -S "examples/example.ll" -o "examples/example.ll.opt"
+
+# Compile unoptimized .ll to executable
 clang examples/*.ll -o examples/example.out
+
+# Run executable
 ./examples/example.out
+
+# Print result of executable
 echo "$?"
