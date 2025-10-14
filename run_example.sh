4207c291fe9598d6a2c45994f106d4cd37219836
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2025-10-14 18:29:38 +0200

    Use clang-18 and opt-18 in run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -4,10 +4,10 @@
 cargo run -- "examples/example.rs"
 
 # Create alternative optimized version of .ll (allows easier debugging of control flow etc.)
-opt -passes=mem2reg,simplifycfg -S "examples/example.ll" -o "examples/example.ll.opt"
+opt-18 -passes=mem2reg,simplifycfg -S "examples/example.ll" -o "examples/example.ll.opt"
 
 # Compile unoptimized .ll to executable
-clang examples/*.ll -o examples/example.out
+clang-18 examples/*.ll -o examples/example.out
 
 # Run executable
 ./examples/example.out
