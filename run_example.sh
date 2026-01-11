aa7b535becf106223366bf8e83932d29184faeb2
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2026-01-11 10:51:49 +0100

    Dynamic vector example

--- run_example.sh
+++ run_example.sh
@@ -3,8 +3,10 @@
 set -e
 
 # Compile .rs to .ll
-cargo run -- "examples/example.mrs" "examples/option.mrs" --build-dir "examples/build" --crate "example"
-
+cargo run -- \
+    --build-dir "examples/build" \
+    --crate "example" \
+    -- "examples/example.mrs" "examples/option.mrs" "examples/vec.mrs"
 # Create alternative optimized version of .ll (allows easier debugging of control flow etc.)
 opt-21 -passes=mem2reg,simplifycfg -S "examples/build/example.ll" -o "examples/build/example.ll.opt"
 
