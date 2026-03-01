00f8204d1741850bd64ead30f9c6a5abcfd7867e
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2026-03-01 17:55:11 +0100

    Remove hlr feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 mod ast;
-#[cfg(feature = "hlr")]
 mod ast_lowering;
 mod ctxt;
 pub mod driver;
-#[cfg(feature = "hlr")]
 mod hlr;
-#[cfg(feature = "hlr")]
 mod hlr_lowering;
 mod mlr_lowering;
 mod parse;
